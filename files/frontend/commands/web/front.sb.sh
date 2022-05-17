5c3e0686edb166467c88eba65b2c9df9ed0cd8ea
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2022-05-17 13:38:24 +0200

    Make front command more universal and not only npm word.

--- files/frontend/commands/web/front.sb.sh
+++ files/frontend/commands/web/front.sb.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 #sb-generated
 
-## Description: [BASE] Run front task build or watch. The possible arguments are build, watch, build-noci, watch-noci. The "no-ci" means that "npm ci" is not run before build command which speed up a lot.
+## Description: [BASE] Run front task build or watch. You can activate option -n (--no-install) which means that no package update is run before build and watch command which speed up a lot.
 ## Usage: front
-## Example: "ddev front build OR ddev front watch OR ddev front build-noci OR ddev front watch-noci"
+## Example: "ddev front build OR ddev front watch OR ddev front build -n OR ddev front watch -n"
 
 TEXT_RED=$(tput setaf 1)
 TEXT_RESET=$(tput sgr0)
+
+NO_INSTALL=0
+if [[ $2 = "-n" ]] || [[ $2 = "--no-install" ]]; then
+    NO_INSTALL=1
+else
+    if [[ ! $2 = "" ]]; then
+        echo "${TEXT_RED}No such option: '$2'. Possible option is -n (--no-install)".
+        exit 1
+    fi
+fi
 
 cd "$PATH_FRONTEND" || exit 1
 
@@ -18,27 +28,25 @@ elif [ "$NVMRC_NODE_VERSION" != "$NODE_VERSION" ]; then
   nvm use
 fi
 
-possibleArguments=(build build-noci watch watch-noci)
+possibleArguments=(build watch)
 if [[ ! ${possibleArguments[*]} =~ $1 ]]; then
     info=$(printf ", %s" "${possibleArguments[@]}")
     info=${info:1}
-    echo "${TEXT_RED}Error! Wrong argument. Possible arguments are: ${info}${TEXT_RESET}"
+    echo "${TEXT_RED}Error! Wrong argument. Possible arguments are:${info}${TEXT_RESET}. If you do not want to install packages add argument -n or --noinstall"
 fi
 
 if [[ $1 = "build" ]]; then
-    npm ci
-    ${FRONTEND_BUILD}
+    if [[ $NO_INSTALL = "1" ]]; then
+        ${FRONTEND_BUILD_NO_PACKAGE_INSTALL}
+    else
+        ${FRONTEND_BUILD}
+    fi
 fi
 
 if [[ $1 = "watch" ]]; then
-    npm ci
-    ${FRONTEND_WATCH}
-fi
-
-if [[ $1 = "build-noci" ]]; then
-    ${FRONTEND_BUILD}
-fi
-
-if [[ $1 = "watch-noci" ]]; then
-    ${FRONTEND_WATCH}
+    if [[ $NO_INSTALL = "1" ]]; then
+        ${FRONTEND_WATCH_NO_PACKAGE_INSTALL}
+    else
+        ${FRONTEND_WATCH}
+    fi
 fi
