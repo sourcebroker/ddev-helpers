3af356df9961c46659e19063d43f60521b140397
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2022-05-24 22:28:43 +0200

    Fix npm command for FRONTNED_PATH.

--- files/frontend/commands/host/npm.sb.sh
+++ files/frontend/commands/host/npm.sb.sh
@@ -5,5 +5,5 @@
 ## Usage: npm [flags] [args]
 ## Example: "ddev npm i"
 
-ddev exec --raw bash -ic "npm '$@'"
+ddev exec --raw bash -ic "TEXT_RED=\$(tput setaf 1) TEXT_RESET=\$(tput sgr0) && echo \"Opening \${FRONTEND_PATH}\" && if [[ -e \"\${FRONTEND_PATH}\" ]]; then cd \"\$FRONTEND_PATH\" && npm '$@'; else echo \"\${TEXT_RED}No FRONTEND_PATH folder: \${FRONTEND_PATH}\${TEXT_RESET}\"; fi;"
 ddev mutagen sync 2>/dev/null
