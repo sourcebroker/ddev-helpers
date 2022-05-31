8f0d45a02624adb4a8e997dfe7864ff3fdbf3f6f
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2022-05-31 10:16:52 +0200

    Add git pull to download script.

--- download.sh
+++ download.sh
@@ -3,5 +3,6 @@ source .ddev-helpers-config
 git clone https://github.com/sourcebroker/ddev-helpers/ .ddev-helpers
 cd .ddev-helpers
 git checkout ${version:-4}
+git pull
 cd bin
 bash ./sb--ddev-hook--pre-start--${system:-typo3}
