ecb0dd2186fcb8217d3097b47ed065eb31bd6d10
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2022-07-02 14:50:48 +0200

    Rm not needed files.

--- files/frontend/commands/host/_todo/npmh.sb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#sb-generated
-
-## Description: [BASE] Run npm command in context of frontend project path (on host side!)
-## Usage: npmh
-## Example: "ddev npmh ci"
-
-source .ddev/commands/host/.fronth_env
-npm $@
