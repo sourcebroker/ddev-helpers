e36235ce990fdbe0e30125ee20bc1da45507d459
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2022-07-05 13:33:32 +0200

    Take latest branch by default.

--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@ cd .ddev
 if [[ -e .ddev-helpers-config ]]; then source .ddev-helpers-config; fi
 if [[ ! -e .ddev-helpers ]]; then git clone https://github.com/sourcebroker/ddev-helpers/ .ddev-helpers; fi
 cd .ddev-helpers
-git fetch --all && git reset --hard origin/${version:-production}
-git checkout ${version:-production}
+git fetch --all && git reset --hard origin/${version:-latest}
+git checkout ${version:-latest}
 cd bin
 bash ./sb--ddev-hook--pre-start--${system:-typo3}
