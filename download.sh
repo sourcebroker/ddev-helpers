345d56b9bef7ecce929630e6c4a6f7edaf3766f2
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2023-02-02 12:47:22 +0100

    Make git quiet.

--- download.sh
+++ download.sh
@@ -1,8 +1,8 @@
 cd .ddev
 if [[ -e .ddev-helpers-config ]]; then source .ddev-helpers-config; fi
-if [[ ! -e .ddev-helpers ]]; then git clone https://github.com/sourcebroker/ddev-helpers/ .ddev-helpers --quiet; fi
+if [[ ! -e .ddev-helpers ]]; then git clone https://github.com/sourcebroker/ddev-helpers/ .ddev-helpers > /dev/null; fi
 cd .ddev-helpers
-git fetch --all --quiet  && git reset --hard origin/${version:-latest} --quiet
-git checkout ${version:-latest} --quiet
+git fetch --all > /dev/null && git reset --hard origin/${version:-latest} > /dev/null
+git checkout ${version:-latest} > /dev/null
 cd bin
 bash ./sb--ddev-hook--pre-start--${system:-typo3}
