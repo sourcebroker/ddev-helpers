b2c8e488f6cd9a8bf76c3f764cd15a5fdbcbd17d
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2024-08-04 18:59:49 +0200

    Refactor download script.

--- download.sh
+++ download.sh
@@ -1,8 +1,8 @@
-cd .ddev
+cd .ddev || exit 1
 if [[ -e .ddev-helpers-config ]]; then source .ddev-helpers-config; fi
-if [[ ! -e .ddev-helpers ]]; then git clone https://github.com/sourcebroker/ddev-helpers/ .ddev-helpers > /dev/null; fi
-cd .ddev-helpers
-git fetch --all > /dev/null && git reset --hard origin/${version:-latest} > /dev/null
-git checkout ${version:-latest} > /dev/null
-cd bin
+if [[ ! -e .ddev-helpers ]]; then git clone --quiet https://github.com/sourcebroker/ddev-helpers/ .ddev-helpers > /dev/null 2>&1; fi
+cd .ddev-helpers || exit 1
+git fetch --all --quiet > /dev/null 2>&1 && git reset --hard origin/${version:-latest} > /dev/null 2>&1
+git checkout ${version:-latest} --quiet > /dev/null 2>&1
+cd bin || exit 1
 bash ./sb--ddev-hook--pre-start--${system:-typo3}
