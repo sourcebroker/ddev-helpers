3b2fd85b545c876438bbd0dd536678186be31c68
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2026-03-15 17:38:49 +0100

    Since ddev 1.24 there is a warning when the name of overwritten command is different than the one in "usage". Therefore npm.sb.sh must be renamed to npm and gitignore with .ddev/commands/web/npm must be added.

--- files/frontend/commands/web/npm.sb.sh
+++ files/frontend/commands/web/npm.sb.sh
@@ -1,18 +1,3 @@
-#!/bin/bash
-#sb-generated
+#sb-generated-deleted
 
-## Description: Run npm inside the web container in the root of the project
-## Usage: npm [flags] [args]
-## Example: "ddev npm i"
-
-TEXT_RED=$(tput setaf 1)
-TEXT_RESET=$(tput sgr0)
-
-if [[ ! -e "${ASSETS_FRONTEND_SRC}" ]]; then echo "${TEXT_RED}No ASSETS_FRONTEND_SRC folder: ${ASSETS_FRONTEND_SRC}${TEXT_RESET}" && exit 1; fi;
-cd "${ASSETS_FRONTEND_SRC}" || exit 1
-
-if [[ ! -e .nvmrc ]]; then echo "${TEXT_RED}No file .nvmrc with node version in folder: ${ASSETS_FRONTEND_SRC}${TEXT_RESET}" && exit 1; fi;
-
-nvm install --latest-npm
-
-npm "$@"
+# Since ddev 1.24 there is a warning when the name of overwritten command is different than in "usage"
